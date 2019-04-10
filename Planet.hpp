532d8d11693ecce8e21cbe435e064c984ea8e485
Diogo Silva <almirante.silva.96@gmail.com>
2019-04-10 19:14:58 +0100

    Finish this file

---
+++ Planet.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+
+
+class Planet {
+    std::string name;
+    float planetAngle, distanceP;
+    glm::vec4 position;
+
+}
